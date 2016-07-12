5b25a2c42ae6d21b2d975d98e4774b1ee0c28925
Yuichiro Hikasa <yhikasa.sfdc@gmail.com>
2016-07-13 06:16:04 +0900

    add delete account

---
+++ web/rest_delete_account.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once 'rest_func.php';
+$access_token = $_SESSION['access_token'];
+$instance_url = $_SESSION['instance_url'];
+if (!isset($access_token) || $access_token == ""
+ || !isset($instance_url) || $instance_url == "") {
+		header('Location: index.html');
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>REST/OAuth サンプル</title>
+    </head>
+    <body>
+        <tt>
+        <a href="rest_show_accounts.php">取引先リスト</a>
+|
+	      <a href="rest_show_account.php">取引先詳細表示</a>
+|
+<a href="rest_create_account.php">取引先作成</a>
+|
+	      <a href="rest_update_account.php">取引先更新</a>
+|
+	      <b>取引先削除</b>
+<br/>
+<form method="post">
+  <input type="text" name="accountId" />
+  <input type="submit" value="削除" />
+</form>
+            <?php
+            $accountId = $_POST['accountId'];
+            if(strlen($accountId) == 0){
+              echo "取引先IDを入力してください";
+            }else{
+              echo $accountId . "を削除します";
+              delete_account($accountId, $instance_url, $access_token);
+            }
+            ?>
+        </tt>
+    </body>
+</html>
